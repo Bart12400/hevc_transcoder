2795c4118b42589c9408ebfb9268cc601f16cf49
Bart12400 <robi.dzialak@gmail.com>
2026-09-01 05:27:59 +0200

    Update DeepMedia Transcoder dependency
    Replaced the dependency for DeepMedia Transcoder with a new API dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,5 +35,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
 
-    implementation("com.otaliastudios:transcoder:0.10.5")
+    // Nowa aktualna zależność DeepMedia Transcoder
+    api("io.deepmedia.community:transcoder:0.10.5")
 }
